df7cc50718d837a6f0dab6e54599779acd9fdc89
onoriodeigbru <onoriode.igbru2024@my.ntu.ac.uk>
2026-04-14 16:03:47 +0100

    Delete pv directory

--- pv/plots/__init__.py
+++
@@ -1,9 +0,0 @@
-from .pv_plots import PVPlotter
-from .wind_plots import WindPlotter
-from .hybrid_plots import HybridPlotter
-
-__all__ = [
-    "PVPlotter",
-    "WindPlotter",
-    "HybridPlotter",
-]
--- pv/scripts/main.py
+++
@@ -1,6 +0,0 @@
-from data_nrel import *
-from data_pvgis import *
-from model import *
-from plots import *
-from validation import *
-
